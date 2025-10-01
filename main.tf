1a68832158a217c595a4c13e3858ccb08f6f3636
Ian Mores <imores@jamey.lu>
2025-10-01 09:40:39 +0000

    removed clout init

--- main.tf
+++ main.tf
@@ -42,22 +42,11 @@ resource "hcloud_server" "k3s_master" {
   user_data = <<-EOT
     #cloud-config
     package_update: true
-    package_upgrade: all
     packages:
-      - apt-transport-https
-      - ca-certificates
-      - curl
-      - gnupg
-      - lsb-release
-    runcmd:
-      - mkdir -p /etc/apt/keyrings
-      - curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-      - echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
-      - apt-get update -y
-      - apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-      - systemctl enable docker
-      - systemctl start docker
+      - python3
+      - python3-apt
   EOT
+
 }
 
 # -------------------------
